e21989e16ef38f8f9024aa274a6d16b32d0de5f0
Masato Shimizu <ma6ato@gmail.com>
2015-06-24 09:29:57 +0900

    owner uuid with namespace

--- nginx/lua/owner_status.lua
+++ nginx/lua/owner_status.lua
@@ -5,7 +5,7 @@ local function getStatus()
     local client = require "redisClient"
     local red, err = client.Connect(31556926)
 
-    local uuid_key = args.keyword .. ":" ..args.token
+    local uuid_key = 'owners:' .. args.keyword .. ':' ..args.token
     local res, err = red:hmget(uuid_key,"uuid")
 
     if not res then

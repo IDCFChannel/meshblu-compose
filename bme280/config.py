3490cc59a5e961385d3be145a03f8c511e97293b
Masato Shimizu <ma6ato@gmail.com>
2015-09-18 15:46:07 +0900

    不要なパラメータを削除

--- bme280/config.py
+++
@@ -1,8 +0,0 @@
-conf = {
-     "IDCF_CHANNEL_URL": "210.140.162.58",
-     "TRIGGER_1_UUID": "21c83792-b25e-4ae7-a627-714af57a1a4b",
-     "TRIGGER_1_TOKEN": "d74ebedf",
-     "ACTION_1_UUID": "3a78814a-6879-4543-bacf-9a206cd951a6",
-     "ACTION_1_TOKEN": "8a781e76",
-     "THRESHOLD": 27.0
-}

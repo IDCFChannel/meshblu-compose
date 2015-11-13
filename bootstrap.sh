19e477a097694b3ed731f787ec8dad03ae59d7d0
Masato Shimizu <ma6ato@gmail.com>
2015-11-13 09:41:44 +0900

    docker-compose 1.5.1

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,7 @@ set -o nounset
 curl -sSL https://get.docker.com/ | sh
 
 # Docker Compose
-curl -L https://github.com/docker/compose/releases/download/1.4.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose; chmod +x /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.5.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose; chmod +x /usr/local/bin/docker-compose
 
 # SSL Certificate
 cd ./nginx/conf/certs
